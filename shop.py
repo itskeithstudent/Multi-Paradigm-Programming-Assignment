7e9dcf3d7921ab5c3790fdf259412d3e2718ed6a
itskeithstudent <G00387816@gmit.ie>
2021-11-22 22:46:44 +0000

    Fix issue with loading in customer, fix data types

--- shop.py
+++ shop.py
@@ -66,10 +66,10 @@ def createAndStockShop(shop_csv):
     with open(shop_csv, "r") as csv_file:
         csv_reader = csv.reader(csv_file,delimiter=',')
         cash = next(csv_reader)[0] #shop's cash is stored in first row so using next() to grab first line from csv_reader
-        shop.cash = float(cash) #set shop's cash
+        shop.cash = float(cash) #set shop's cash, cast as float so not a string
         for row in csv_reader:
-            product = Product(name=row[0], price=row[1]) #set product details
-            product_stock = ProductStock(product=product,quantity=row[2]) #set product stock details
+            product = Product(name=row[0], price=float(row[1])) #set product details, enforce float type for price
+            product_stock = ProductStock(product=product,quantity=int(row[2])) #set product stock details, enforce type of int for quantity
             shop.stock.append(product_stock) #append product_stock to shop's stock list
     return shop
 
@@ -85,18 +85,20 @@ def createCustomerOrder(customer_csv):
 
     with open(customer_csv, "r") as csv_file:
         csv_reader = csv.reader(csv_file,delimiter=',')
-        budget = next(csv_reader)[0] #customer's budget is stored in first row so using next() to grab first line from csv_reader
-        customer.budget = budget #set shop's cash
+        #budget = float(next(csv_reader)[0])
+        first_row = next(csv_reader) #customer's name and budget is stored in first row so using next() to grab first line from csv_reader
+        customer.name = first_row[0]
+        customer.budget = float(first_row[1]) #set shop's cash
         for row in csv_reader:
             product = Product(name=row[0], price=0.0) #set product details, customer doesn't know items price so leaving as 0.0 (inflation is very high these days, hard to keep track of price of things!)
-            product_quantity = ProductStock(product=product,quantity=row[1]) #set ProductStock item to be appended to customers shopping list
+            product_quantity = ProductStock(product=product,quantity=int(row[1])) #set ProductStock item to be appended to customers shopping list
             customer.shopping_list.append(product_quantity) #append product_stock to shop's stock list
     return customer
 
 if __name__ == '__main__':
     shop = createAndStockShop('Shop Stock\\stock.csv')
     printShop(shop)
-    #customer = createCustomerOrder('Customer Orders\\customer_order_a.csv')
+    customer = createCustomerOrder('Customer Orders\\customer_order_a.csv')
     # print("\nPlease choose an option:\n(1) Normal customer order\n(2) Customer order with not enough money\n(3) Customer order with excess quantity\n(4) Live Order\n(5) Check shop stock and balance\n(0) Exit Shop\n")
     # selection = ''
     # while selection != "0":
