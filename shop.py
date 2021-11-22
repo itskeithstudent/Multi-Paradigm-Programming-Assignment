674125b65df68f06be8b3bd49119f39ac7fdde6a
itskeithstudent <G00387816@gmit.ie>
2021-11-22 23:27:11 +0000

    Add functions to update shop stock, check stock and get product price

--- shop.py
+++ shop.py
@@ -95,10 +95,71 @@ def createCustomerOrder(customer_csv):
             customer.shopping_list.append(product_quantity) #append product_stock to shop's stock list
     return customer
 
+def check_stock(shop, product_name, product_quantity):
+    '''
+        check_stock - checks a shop dataclass for a product and the quantity it has in stock of that product
+        parameters:
+        shop (dataclass) - dataclass for the shop to have its stock checked
+        product_name (string) - name of the product to be checked
+        product_quantity (int) - quantity of the product being sought
+        returns:
+        requested quantity if shop has enough stock
+        maximum quantity in the shop if it doesn't have enough stock
+        0 otherwise
+    '''
+    #loop through items in shop stock
+    for i in shop.stock:
+        if product_name == i.product.name:
+            print(f"Checking shop stock of {i.product.name}, quantity in stock - {i.quantity}\n")
+            if i.quantity >= product_quantity:
+                return product_quantity
+            else:
+                print(f"Updating quantity to match what shop has in stock - {i.quantity}\n")
+                return i.quantity
+    #if have gone through entire shop stock and no match, then shop doesn't stock item, return 0 for this item
+    print("We don't stock this item.")
+    return 0
+
+def findPrice(shop, product_name):
+    '''
+        findPrice - checks a shop dataclass for a product and returns the price of it if it has that product in stock
+        parameters:
+        shop (dataclass) - dataclass for the shop to have its stock checked
+        product_name (string) - name of the product to be checked
+        returns:
+        price of the requested product
+    '''
+    for i in shop.stock:
+        if i.product.name == product_name:
+            product_price = i.product.price
+            print(product_price)
+            return product_price
+    #in-case nothing found
+    print("Product not found in shop stock, please check for any typos and try again.")
+
+def update_shop(shop, product_name, quantity_removed):
+    '''
+        update_shop - updates the quantity of a product in a shop dataclass in-place
+        parameters:
+        shop (dataclass) - dataclass for the shop to have its stock updated
+        product_name (string) - name of the product who's quantity is to be updated
+        quantity_removed (int) - quantity of the product being updated in shop
+        returns:
+        N/A, shop is updated in-place
+    '''
+    for i in shop.stock:
+        if i.product.name == product_name:
+            i.quantity = i.quantity - quantity_removed
+
 if __name__ == '__main__':
     shop = createAndStockShop('Shop Stock\\stock.csv')
     printShop(shop)
     customer = createCustomerOrder('Customer Orders\\customer_order_a.csv')
+    
+    #findPrice(shop, "Coke Can")
+    #check_stock(shop, "Coke Can", 1000)
+    #update_shop(shop, "Coke Can", 100)
+    #printShop(shop)
     # print("\nPlease choose an option:\n(1) Normal customer order\n(2) Customer order with not enough money\n(3) Customer order with excess quantity\n(4) Live Order\n(5) Check shop stock and balance\n(0) Exit Shop\n")
     # selection = ''
     # while selection != "0":
